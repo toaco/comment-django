fe229b0842cdc4d3eee294f18b9fbde7a7a9ad99
Jeffrey <Jeffrey.S.Teo@gmail.com>
2017-01-31 18:03:41 +0800

    conf/__init__.py:阅读了setting的延迟实例化的使用和实例化过程

--- __init__.py
+++ __init__.py
@@ -7,10 +7,11 @@ __version__ = get_version(VERSION)
 
 def setup():
     """
+    初始化Django
     Configure the settings (this happens as a side effect of accessing the
     first setting), configure logging and populate the app registry.
     """
-    # TODO: 为什么在函数内部导入
+    # TODO: 为什么在函数内部导入,减少导入模块的时间,不调用不导入.
     from django.apps import apps
     from django.conf import settings
     from django.utils.log import configure_logging
--- core/wsgi.py
+++ core/wsgi.py
@@ -4,6 +4,7 @@ from django.core.handlers.wsgi import WSGIHandler
 
 def get_wsgi_application():
     """
+    先初始化Django,然后返回WSGI需要的应用对象
     The public interface to Django's WSGI support. Should return a WSGI
     callable.
 
